a3aaa04940c36ce2ed8d8ff997966418bd993dec
vinko006 <vinko.situm1@fpmoz.sum.ba>
2025-07-07 14:14:59 +0200

    Add files via upload

---
+++ ntorke.py
@@ -0,0 +1,41 @@
+"""
+Iz podataka učitanih u prethodnom primjeru sortirati listu po prezimenima.
+Napraviti novi rječnik gdje će se po bodovnom rangu
+upisivati broj ostvarenih ocjena. 
+"""
+
+rezultati = []
+
+with open("rezultati.csv", "r", encoding="utf-8") as file:
+    for linija in file:
+        ime, prezime, bodovi = linija.strip().split(",")
+        rezultati.append((ime, prezime, int(bodovi)))
+
+for ime, prezime, bodovi in rezultati:
+    if bodovi > 49:
+        print(ime, prezime, bodovi)
+
+rezultati.sort(key=lambda x: x[1])
+
+ocjene = {
+    "Nedovoljan": 0,
+    "Dovoljan": 0,
+    "Dobar": 0,
+    "Vrlo dobar": 0,
+    "Odličan": 0
+}
+
+for ime, prezime, bodovi in rezultati:
+    if bodovi < 50:
+        ocjene["Nedovoljan"] += 1
+    elif bodovi < 65:
+        ocjene["Dovoljan"] += 1
+    elif bodovi < 80:
+        ocjene["Dobar"] += 1
+    elif bodovi < 90:
+        ocjene["Vrlo dobar"] += 1
+    else:
+        ocjene["Odličan"] += 1
+
+for ocjena in ocjene:
+    print(ocjena, ocjene[ocjena])
